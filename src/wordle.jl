ad316c05dd59c563737ec044821aacc1dca42654
Karl Linek <karl@linek.at>
2024-10-05 02:49:50 +0200

    added function make_word_vektor

--- src/wordle.jl
+++ src/wordle.jl
@@ -1,5 +1,19 @@
 module wordle
 
-greet() = print("Hello World!")
+using JSON3
+
+export make_word_vector, word_list_path
+
+
+word_list_path = "data/raw/wortliste.json"
+
+function make_word_vector(json_path::String, length_of_words::Int)
+	file = open(json_path, "r")
+	words = JSON3.read(file)
+	close(file)
+	word_vector = filter(word -> length(word) == length_of_words, words)
+
+	return word_vector
+end
 
 end # module wordle
