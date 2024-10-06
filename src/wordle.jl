a1a6e18a7447755d1388ca2a64de192b375240e6
Karl Linek <karl@linek.at>
2024-10-06 02:23:32 +0200

    added function cleanword

--- src/wordle.jl
+++ src/wordle.jl
@@ -2,7 +2,7 @@ module wordle
 
 using JSON3
 
-export make_word_vector, word_list_path
+export make_word_vector, word_list_path, cleanword
 
 
 word_list_path = "data/raw/wortliste.json"
@@ -16,4 +16,26 @@ function make_word_vector(json_path::String, length_of_words::Int)
 	return word_vector
 end
 
-end # module wordle
+function cleanword(input::AbstractString)
+	changetable = Dict(
+		'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'å' => 'a', 'ä' => 'a',
+		'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+		'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
+		'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ø' => 'o', 'ö' => 'o',
+		'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
+		'ç' => 'c',
+		'ñ' => 'n', 'Ñ' => 'N'
+	)
+
+	output = ""
+	for char in input
+		output *= haskey(changetable, char) ? changetable[char] : char
+	end
+	return output
+end
+
+end # module word
+
+
+#word_vector = make_word_vector(word_list_path, 5)
+#println(word_vector)
